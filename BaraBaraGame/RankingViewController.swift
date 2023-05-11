5cf22a5516415815d6975d06d3ecdf2151800c0f
SotaShirakawa <souta060614@gmail.com>
2023-05-12 08:55:31 +0900

    すっきりさせた

--- BaraBaraGame/RankingViewController.swift
+++ BaraBaraGame/RankingViewController.swift
@@ -15,7 +15,7 @@ class RankingViewController: UIViewController {
     
     let saveData: UserDefaults = UserDefaults.standard
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,15 +29,15 @@ class RankingViewController: UIViewController {
         self.dismiss(animated: true, completion:  nil)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
